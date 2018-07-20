5b52ed65135426cde20993f536f4f137fc011d15
Dean Tambling <dean.tambling@gmail.com>
2018-07-20 14:33:28 -0700

    Tune up failure/error comments

--- src/helpers/commentHelpers/failedComment.js
+++ src/helpers/commentHelpers/failedComment.js
@@ -1,11 +1,16 @@
 const { getFailures } = require('../../parsers/JestParser')
 
-const constructFailedComment = ({audience, sha, log, link}) => `
-Hey @${audience}, looks like your commit at ${sha} had some failing tests: 
+const constructFailedComment = ({audience, sha, log, link}) => {
+  const failures = getFailures(log)
+  const plural = failures.length > 1
+
+  return `
+Hey @${audience}, looks like your commit at ${sha} had ${plural ? 'some' : 'a'} failing test${plural && 's'}: 
 <ul>
 ${getFailures(log).map(line => `<li><code>${line}</code></li>`).join('\n')}
 </ul> 
-You might want to get those tests passing and update this PR. For more details, you can see the whole build log <a href="${link}" target="_blank">here</a>
+You might want to get ${plural ? 'those' : 'that'} test${plural && 's'} passing and update this PR. For more details, you can see the whole build log <a href="${link}" target="_blank">here</a>.
 `
+}
 
 module.exports = constructFailedComment

336dc335c33ba23d5bf4706715755021bca36b2b
Dean Tambling <dean.tambling@gmail.com>
2018-07-20 16:38:42 -0700

    Polish up failing tests message

--- src/helpers/commentHelpers/failedComment.js
+++ src/helpers/commentHelpers/failedComment.js
@@ -5,11 +5,14 @@ const constructFailedComment = ({audience, sha, log, link}) => {
   const plural = failures.length > 1
 
   return `
-Hey @${audience}, looks like your commit at ${sha} had ${plural ? 'some' : 'a'} failing test${plural && 's'}: 
+Hey @${audience}, looks like your commit at ${sha} had ${plural ? 'some' : 'a'} failing test${plural ? 's' : ''}: 
 <ul>
-${getFailures(log).map(line => `<li><code>${line}</code></li>`).join('\n')}
+${failures.map(line => `<li><code>${line}</code></li>`).join('\n')}
 </ul> 
-You might want to get ${plural ? 'those' : 'that'} test${plural && 's'} passing and update this PR. For more details, you can see the whole build log <a href="${link}" target="_blank">here</a>.
+You might want to get ${plural ? 'those' : 'that'} test${plural ? 's' : ''} passing and update this PR. 
+<br />
+<br />
+For more details, you can see the whole build log <a href="${link}" target="_blank">here</a>.
 `
 }
 

ce1d9ec669a108555086ad0f7cf5d4772878702f
Dean Tambling <dean.tambling@gmail.com>
2018-07-21 23:38:23 -0700

    Test promiseHelper

---
+++ test/src/helpers/promiseHelpers.test.js
@@ -0,0 +1,39 @@
+const { pollPromise } = require('../../../src/helpers/promiseHelpers')
+
+describe('pollPromise', () => {
+  const initiate = () => new Promise((resolve) => resolve())
+
+  it('resolves with a value it checkCandidate returns true', async () => {
+    const getCandidate = () => 'Resolved'
+    const checkCandidate = () => true
+
+    const result = await pollPromise({initiate, getCandidate, checkCandidate})
+
+    expect(result).toEqual('Resolved')
+  });
+
+  it('rejects if it exceeds the maximum tries', () => {
+    const getCandidate = () => false
+    const checkCandidate = () => false
+    const maxTries = 1
+    const wait = 0
+
+    pollPromise({initiate, getCandidate, checkCandidate, maxTries, wait})
+      .catch((err) => {
+        expect(err).toEqual(new Error('Maximum number of tries exceeded'))
+      
+      })
+  })
+  it('sets a timeout and tries again', () => {
+    const getCandidate = () => 'Resolved'
+    const checkCandidate = jest.fn().mockReturnValueOnce(false).mockReturnValue(true) 
+    const maxTries = 2
+    const wait = 0
+
+    pollPromise({initiate, getCandidate, checkCandidate, maxTries, wait})
+      .then((result) => {
+        expect(result).toBe('Resolved')
+        expect(checkCandidate).toHaveBeenCalledTimes(2)
+      })
+  })
+})

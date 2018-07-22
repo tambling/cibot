29db52d15fd3a9952b10a2f5deb3c8cd12c68142
Dean Tambling <dean.tambling@gmail.com>
2018-07-21 19:26:17 -0700

    Test travisClient

---
+++ test/src/clients/travisClient.test.js
@@ -0,0 +1,92 @@
+process.env['TRAVIS_TOKEN'] = 'hello'
+
+const fetch = require('node-fetch')
+const travisClient = require('../../../src/clients/travisClient')
+
+jest.mock('node-fetch')
+const mockedJsonParse = jest.fn().mockResolvedValue({foo: 'bar'})
+fetch.mockResolvedValue({ json: mockedJsonParse })
+
+describe('travisClient', () => {
+  afterEach(() => fetch.mockClear())
+
+  describe('get', () => {
+    it('fetches the path passed in', () => {
+      travisClient.get('this/path')
+
+      const expectedUrl = 'https://api.travis-ci.com/this/path'
+
+      expect(fetch.mock.calls[0][0]).toEqual(expectedUrl)
+    })
+
+    it('sets the headers properly', () => {
+      const expectedHeaders = {
+        'Travis-API-Version': '3',
+        'Authorization': 'token hello'
+      }
+
+      travisClient.get('this/path')
+
+      expect(fetch.mock.calls[0][1]).toEqual({ headers: expectedHeaders })
+    })
+    it('returns the parsed JSON', async () => {
+      const actualReturn = await travisClient.get('this/path')
+
+      expect(actualReturn).toEqual({foo: 'bar'})
+    })
+  })
+
+  describe('getRepoBuilds', () => {
+    it('fetches the builds for the proper repo', () => {
+      travisClient.getRepoBuilds('user/repo')
+
+      const expectedUrl = 'https://api.travis-ci.com/repo/user%2frepo/builds'
+
+      expect(fetch.mock.calls[0][0]).toEqual(expectedUrl)
+    })
+
+    it('sets the headers properly', () => {
+      const expectedHeaders = {
+        'Travis-API-Version': '3',
+        'Authorization': 'token hello'
+      }
+
+      travisClient.getRepoBuilds('user/repo')
+
+      expect(fetch.mock.calls[0][1]).toEqual({ headers: expectedHeaders })
+    })
+
+    it('returns the parsed JSON', async () => {
+      const actualReturn = await travisClient.getRepoBuilds('user/repo')
+
+      expect(actualReturn).toEqual({foo: 'bar'})
+    })
+  })
+
+  describe('getLog', () => {
+    it('fetches the logs for the proper job', () => {
+      travisClient.getLog(1)
+
+      const expectedUrl = 'https://api.travis-ci.com/job/1/log'
+
+      expect(fetch.mock.calls[0][0]).toEqual(expectedUrl)
+    })
+
+    it('sets the headers properly', () => {
+      const expectedHeaders = {
+        'Travis-API-Version': '3',
+        'Authorization': 'token hello'
+      }
+
+      travisClient.getLog(1)
+
+      expect(fetch.mock.calls[0][1]).toEqual({ headers: expectedHeaders })
+    })
+
+    it('returns the parsed JSON', async () => {
+      const actualReturn = await travisClient.getLog(1)
+
+      expect(actualReturn).toEqual({foo: 'bar'})
+    })
+  })
+})

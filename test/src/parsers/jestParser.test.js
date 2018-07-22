42493ac19bc7161696c50881d0754fbd0245db24
Dean Tambling <dean.tambling@gmail.com>
2018-07-21 23:20:53 -0700

    Test jestParser

---
+++ test/src/parsers/jestParser.test.js
@@ -0,0 +1,16 @@
+const { getFailures, getErrors } = require('../../../src/parsers/jestParser')
+const { failLog, errorLog } = require('../../fixtures/logExcerpts')
+
+describe('jestParser', () => {
+  describe('getFailures', () => {
+    it('parses failures out of a raw log', () => {
+      expect(getFailures(failLog)).toEqual(['returns 1 when the power is 0 (13ms)'])
+    })
+  })
+
+  describe('getErrors', () => {
+    it('parses errors out of a raw log', () => {
+      expect(getErrors(errorLog)).toEqual(["404 Not Found: fake-package-that-doesnt-exist@^0.0.1"])
+    })
+  })
+})
